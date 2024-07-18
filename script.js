e3a34912773ac8cd2d03aaa6b992f02e6c84a878
Marius <goldinovas.marius@gmail.com>
2024-07-18 13:48:29 +0300

    dom

--- script.js
+++ script.js
@@ -7,7 +7,32 @@ document.addEventListener('DOMContentLoaded', function() {
         event.preventDefault();
 
         h1Element.textContent = 'About';
-        pElement.textContent = 'kakkkkkkkkkkkkkkkkkkkkkkkkkkkkadu';
+        pElement.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, sequi possimus ad quod officia maxime! Ullam optio amet est laborum reprehenderit adipisci minima, consequatur';
     });
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    const historyLink = document.getElementById('history-link');
+    const h1Element = document.getElementById('h1-keisis');
+    const pElement = document.getElementById('p-keisis');
+
+    historyLink.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        h1Element.textContent = 'History';
+        pElement.textContent = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, sequi possimus ad quod officia maxime! Ullam optio amet est laborum reprehenderit adipisci minima, consequatur';
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const contactsLink = document.getElementById('contacts-link');
+    const h1Element = document.getElementById('h1-keisis');
+    const pElement = document.getElementById('p-keisis');
+
+    contactsLink.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        h1Element.textContent = 'Contacts';
+        pElement.textContent = 'kontaktai';
+    });
+});
